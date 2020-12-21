4530e08981f5ca6177c75d2c19ad57656c2d07d6
Técnico Montevideo <tecnicomvd@sanmiguel.local>
2020-12-21 14:43:08 -0300

    asdasd

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
         </p>
         <h2>Nachito sabeeeeeeeeeeeeee</h2>
         <p>Se cambio ruta del logo, esto tendria que estar funcionando</p>
+        <p>Se cambio ruta del logo, esto tendria que estar funcionando</p>
+        <p>Se cambio ruta del logo, esto tendria que estar funcionando</p>
         <a
           className="App-link"
           href="https://reactjs.org"
